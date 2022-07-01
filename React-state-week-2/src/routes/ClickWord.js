e7730b346c2e80f02e1e1f8b2baa03ba6440dcf1
ediiotero <e.otero81@gmail.com>
2022-06-30 21:53:11 -0400

    test week 2

---
+++ React-state-week-2/src/routes/ClickWord.js
@@ -0,0 +1,16 @@
+//Write a component WordProp that will take a prop of 'word'
+// Make a 2nd component ClickWord that will use the WordProp component
+// to display a word from its state
+// -Make 2 buttons that will change the state (change the current word)
+// and display the new word in the DOM
+
+import React, { Component } from 'react'
+
+export default class ClickWord extends Component {
+
+  render() {
+    return (
+      <div>ClickWord</div>
+    )
+  }
+}
---
+++ React-state-week-2/src/routes/PropSample.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PropSample = props => {
+   return (
+    <div style={{
+        fontSize: '5rem',
+        color: 'red'
+    }}>
+       
+    </div>
+   )
+}
+
+export default PropSample
